2c567d38b93bda95609f71e9982e9778b77cac67
Somiya Kumari <somiyakumari2353@gmail.com>
2025-08-17 00:59:22 +0530

    Learnt API with Django

--- drf-project/watchmate/watchlist_app/views.py
+++ drf-project/watchmate/watchlist_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse,Http404
+from watchlist_app.models import Movie
 
-# Create your views here.
+def movies_list(request):
+    movies = Movie.objects.all()
+    #return HttpResponse(movies)  # will print queryset as text
+    #movies.values()=query set of dictionary
+    data={'movies' : list(movies.values())}
+    return JsonResponse(data)
+def movie_detail(request, pk):
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        raise Http404("Movie not found")
+    
+    data = {
+        "name": movie.name,
+        "description": movie.description,
+    }
+    return JsonResponse(data)
